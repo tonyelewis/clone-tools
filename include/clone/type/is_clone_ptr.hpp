53522f9d8aa2e33e5fb1209ee79d9e1a0aeddce4
Tony Lewis <tonyelewis@users.noreply.github.com>
2016-12-08 16:52:03 +0000

    Tidy is_clone_ptr/is_clone_ptr (mostly comments)

--- include/clone/type/is_clone_ptr.hpp
+++ include/clone/type/is_clone_ptr.hpp
@@ -13,37 +13,31 @@ namespace clone_tools { template <typename T> class clone_ptr; }
 
 namespace clone_tools {
 
-	/// \brief Whether some type is a `clone_tools::clone_ptr<...>`
+	/// \brief Type trait struct for whether T is a `clone_tools::clone_ptr<...>`
 	///
-	/// Note that this is strict: it only evaluates to true for `clone_ptr< ... >`; it's false for:
-	///  *       `clone_ptr< ... > &`
-	///  * `const clone_ptr< ... >`
-	///  * `const clone_ptr< ... > &`
-	///
-	/// For a less strict version, use is_clone_ptr_after_decay
+	/// For a less strict version that ignores any const/volatile/reference, use is_clone_ptr_after_decay
 	template <typename T> struct is_clone_ptr : detail::is_template_of_type< T, ::clone_tools::clone_ptr > {};
 
-	/// \brief Whether some type is a `clone_tools::clone_ptr<...>`
+	/// \brief Type trait struct for whether T is a `clone_tools::clone_ptr<...>` after any const/volatile/reference is stripped off
 	///
-	/// This non-strict version is true for all of:
-	///  *       `clone_ptr< ... >`
-	///  *       `clone_ptr< ... > &`
-	///  * `const clone_ptr< ... >`
-	///  * `const clone_ptr< ... > &`
+	/// For a stricter version that rejects if T has any const/volatile/reference, use is_clone_ptr
 	template <typename T> struct is_clone_ptr_after_decay : detail::is_template_of_type< detail::decay_t< T >, ::clone_tools::clone_ptr > {};
 
 #ifdef __cpp_variable_templates
 
-	/// \brief Variable template for whether some type is a `clone_tools::clone_ptr<...>`
+	/// \brief Variable template for whether T is a `clone_tools::clone_ptr<...>`
+	///
+	/// For a less strict version that ignores any const/volatile/reference, use is_clone_ptr_after_decay_v
 	template <typename T>
 	static constexpr bool is_clone_ptr_v             = is_clone_ptr            <T>::value;
 
-	/// \brief Variable template for whether some type is a `clone_tools::clone_ptr<...>`
+	/// \brief Variable template for whether T is a `clone_tools::clone_ptr<...>` after any const/volatile/reference is stripped off
+	///
+	/// For a stricter version that rejects if T has any const/volatile/reference, use is_clone_ptr_v
 	template <typename T>
 	static constexpr bool is_clone_ptr_after_decay_v = is_clone_ptr_after_decay<T>::value;
 
 #endif // __cpp_variable_templates
-
 
 } // namespace clone_tools
 
--- include/clone/type/is_unique_ptr.hpp
+++ include/clone/type/is_unique_ptr.hpp
@@ -13,32 +13,27 @@
 
 namespace clone_tools {
 
-	/// \brief Variable template for whether some type is a `std::unique_ptr<...>`
+	/// \brief Type trait struct for whether T is a `std::unique_ptr<...>`
 	///
-	/// Note that this is strict: it only evaluates to true for `unique_ptr< ... >`; it's false for:
-	///  *       `unique_ptr< ... > &`
-	///  * `const unique_ptr< ... >`
-	///  * `const unique_ptr< ... > &`
-	///
-	/// For a less strict version, use is_unique_ptr_after_decay
-	template <typename T> struct is_unique_ptr : detail::is_template_of_type< T, std::unique_ptr > {};
+	/// For a less strict version that ignores any const/volatile/reference, use is_unique_ptr_after_decay
+	template <typename T> struct is_unique_ptr : detail::is_template_of_type< T, ::std::unique_ptr > {};
 
-	/// \brief Variable template for whether some type is a `std::unique_ptr<...>`
+	/// \brief Type trait struct for whether T is a `std::unique_ptr<...>` after any const/volatile/reference is stripped off
 	///
-	/// This non-strict version is true for all of:
-	///  *       `unique_ptr< ... >`
-	///  *       `unique_ptr< ... > &`
-	///  * `const unique_ptr< ... >`
-	///  * `const unique_ptr< ... > &`
-	template <typename T> struct is_unique_ptr_after_decay : detail::is_template_of_type< detail::decay_t< T >, std::unique_ptr > {};
+	/// For a stricter version that rejects if T has any const/volatile/reference, use is_unique_ptr
+	template <typename T> struct is_unique_ptr_after_decay : detail::is_template_of_type< detail::decay_t< T >, ::std::unique_ptr > {};
 
 #ifdef __cpp_variable_templates
 
-	/// \brief Variable template for whether some type is a `std::unique_ptr<...>`
+	/// \brief Variable template for whether T is a `std::unique_ptr<...>`
+	///
+	/// For a less strict version that ignores any const/volatile/reference, use is_unique_ptr_after_decay_v
 	template <typename T>
 	static constexpr bool is_unique_ptr_v             = is_unique_ptr            <T>::value;
 
-	/// \brief Variable template for whether some type is a `std::unique_ptr<...>`
+	/// \brief Variable template for whether T is a `std::unique_ptr<...>` after any const/volatile/reference is stripped off
+	///
+	/// For a stricter version that rejects if T has any const/volatile/reference, use is_unique_ptr_v
 	template <typename T>
 	static constexpr bool is_unique_ptr_after_decay_v = is_unique_ptr_after_decay<T>::value;
 
